a0e6a8a5e84d0507dca519bea052fefab4f58ab2
Anya Bu <annabukhtiiarova@gmail.com>
2022-07-11 16:30:52 +0300

    first

--- src/check.js
+++ src/check.js
@@ -1,1 +1,17 @@
 import { hotel } from './index.js';
+
+hotel.getLength();
+hotel.checkInGuest('a','b',20)
+hotel.checkInGuest('a','b',20)
+
+
+
+console.log(hotel.guests)
+//количество гостей
+console.log(hotel.getLength())
+//количество свободных мест
+console.log(hotel.getActualFreePlace())
+//заселяем гостей
+
+
+console.log(hotel)
--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-export const hotel = new Object();
+export const hotel = {
+    quantityOfPlaces: 30,
+    priceByPlace: 20,
+    bankAccount: 0,
+    guests: {},
+    getLength() {
+        return (Object.keys(this.guests)).length
+    },
+    getActualFreePlace() {
+        return this.quantityOfPlaces - this.getLength()
+    },
+
+    checkInGuest(firstName, lastName, money) {
+
+        let newGuest = {
+                    firstName,
+                    lastName,
+                    money
+        }
+
+        if (this.getActualFreePlace() <= 0){
+            return 'Sorry, we have not free spaces'
+        } else if (newGuest.money < this.priceByPlace) {
+            return 'Sorry, you have not enough money'
+        } else {
+            newGuest.money = money - hotel.priceByPlace
+            this.bankAccount =  this.bankAccount + this.priceByPlace;
+
+            let newGuestId = Object.keys(this.guests).length + 1
+
+            this.guests = Object.assign(this.guests, {[newGuestId]:newGuest})
+
+        }
+
+    }
+
+
+};
+
