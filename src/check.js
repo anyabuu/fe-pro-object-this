c694e1134e68f937abc4fd07624471d2c1b5b5e0
Anya Bu <annabukhtiiarova@gmail.com>
2022-07-12 13:28:47 +0300

    prettier

--- src/check.js
+++ src/check.js
@@ -1,18 +1,15 @@
 import { hotel } from './index.js';
 
-
-hotel.checkInGuest('Mijku','Muk',40)
+hotel.checkInGuest('Mijku', 'Muk', 40);
 
 console.log(hotel.getLength());
 console.log(hotel.bankAccount);
 
-
-console.log(hotel.guests)
+console.log(hotel.guests);
 //количество гостей
-console.log(hotel.getLength())
+console.log(hotel.getLength());
 //количество свободных мест
-console.log(hotel.getActualFreePlace())
+console.log(hotel.getActualFreePlace());
 //заселяем гостей
 
-
-console.log(hotel)
+console.log(hotel);
--- src/index.js
+++ src/index.js
@@ -1,39 +1,33 @@
 export const hotel = {
-    quantityOfPlaces: 30,
-    priceByPlace: 20,
-    bankAccount: 0,
-    guests: {},
-    getLength() {
-        return (Object.keys(this.guests)).length
-    },
-    getActualFreePlace() {
-        return this.quantityOfPlaces - this.getLength()
-    },
+  quantityOfPlaces: 30,
+  priceByPlace: 20,
+  bankAccount: 0,
+  guests: {},
+  getLength() {
+    return Object.keys(this.guests).length;
+  },
+  getActualFreePlace() {
+    return this.quantityOfPlaces - this.getLength();
+  },
 
-    checkInGuest(firstName, lastName, money) {
-
-        function Guest(firstName, lastName, money) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.money = money - hotel.priceByPlace;
-        }
-
-        if (money < this.priceByPlace) {
-            return 'Sorry, you have not enough money'
-        } else if (this.getActualFreePlace() <= 0){
-                return 'Sorry, we have not free spaces'
-        }
-
-        this.bankAccount =  this.bankAccount + this.priceByPlace;
-        return this.guests[this.getLength()] = new Guest(firstName, lastName, money);
+  checkInGuest(firstName, lastName, money) {
+    function Guest(firstName, lastName, money) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.money = money - hotel.priceByPlace;
     }
 
+    if (money < this.priceByPlace) {
+      return 'Sorry, you have not enough money';
+    } else if (this.getActualFreePlace() <= 0) {
+      return 'Sorry, we have not free spaces';
+    }
+
+    this.bankAccount = this.bankAccount + this.priceByPlace;
+    return (this.guests[this.getLength()] = new Guest(
+      firstName,
+      lastName,
+      money
+    ));
+  },
 };
-
-
-
-
-
-
-
-
